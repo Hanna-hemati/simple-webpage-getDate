a898b39b66e24128049c998510c358643cd3e8df
Hanna <102343113+Hanna-hemati@users.noreply.github.com>
2022-07-31 19:21:50 +0430

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
-var today = new Date().getDay();
+  var now  = new Date();        // current date/time
+    var hrs  = now.getHours();    // 0 to 23
+    var mins = now.getMinutes();
+    var secs = now.getSeconds();
+	
+	
+	var weekday = new Array(7);
+weekday[0]=  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+var day = weekday[now.getDay()];
 
-if ((today = 0)) {
-  document.getElementById("today").innerHTML = "sunday";
-} else if ((today = 1)) {
-  document.getElementById("today").innerHTML = "monday";
-} else if ((today = 2)) {
-  document.getElementById("today").innerHTML = "tusday";
-} else if ((today = 3)) {
-  document.getElementById("today").innerHTML = "thersday";
-} else if ((today = 4)) {
-  document.getElementById("today").innerHTML = "wensday";
-} else if ((today = 5)) {
-  document.getElementById("today").innerHTML = "friday";
-} else {
-  document.getElementById("today").innerHTML = "saterday";
-}
+
+    document.writeln("<p>It is " + now + "</p>");
+    document.writeln("<p>Hour is " + hrs + "</p>");
+    document.writeln("<p>Minute is " + mins + "</p>");
+    document.writeln("<p>Second is " + secs + "</p>");
+	document.writeln("<p>Today is " + day + "</p>");
+    if (hrs < 12) {
+      document.writeln("<h2>Good Morning!</h2>");
+    } else {
+      document.writeln("<h2>Good Afternoon!</h2>");
+    }
+	if (day = "Sunday") {
+      document.writeln("<h2>Sunday</h2>");
+    } 
+	else if (day = "Monday"){
+      document.writeln("<h2>Monday</h2>");
+    }
+	 else {
+      document.writeln("<h2>n</h2>");
+    }
